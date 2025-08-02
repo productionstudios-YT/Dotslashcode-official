5e12122b445687181408b301a1b7f542f728443f
productionstudios-YT <productionstudio2011@gmail.com>
2025-08-02 10:48:09 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,15 @@
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('dotslash-store').then(function(cache) {
+      return cache.addAll(['index.html', 'manifest.json', 'icon.png']);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
